3021ab2004aed3c70b77c69412c56378412189bb
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-10-22 16:43:31 +0530

    First Commit

---
+++ src/main/java/com/uor/ecommerce/security/jwt/JwtUtils.java
@@ -0,0 +1,33 @@
+package com.uor.ecommerce.security.jwt;
+
+// Utility class for JWT for generating and validating JWT
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
+
+@Component
+@Slf4j
+public class JwtUtils {
+
+  @Value("${spring.app.jwtSecret}")
+  private String jwtSecret;
+
+  @Value("${spring.app.jwtExpirationMs}")
+  private int jwtExpirationMs;
+
+  @Value("${spring.ecom.app.jwtCookieName}")
+  private String jwtCookie;
+
+  public String getJwtFromCookies(HttpServletRequest request) {
+    Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+    if (cookie != null) {
+      return cookie.getValue();
+    } else {
+      return null;
+    }
+  }
+}
